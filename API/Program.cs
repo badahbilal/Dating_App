74d552353d0ca5d3d9c3aa14f783d4bd3c32be4b
bbadah <bilal.badah@capgemini.com>
2023-06-05 13:21:19 +0100

    8. Getting to know the API project files

--- API/Program.cs
+++ API/Program.cs
@@ -3,23 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+/*
+Now we have two sections, if you like, inside this file.
+Anything before app build app equals builder dot build is considered our services container.
+Any services that we add, we add to this container.
+*/
 var app = builder.Build();
 
+/*
+And then the next part of this below this comment is what's referred to as the HTTP request pipeline.
+*/
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
