22d09dba90685a6ec53ed85b79412a3f94ca950c
bbadah <bilal.badah@capgemini.com>
2023-06-05 15:33:01 +0100

    12. Adding a DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// This block of code registers the DataContext class as a service in the dependency injection container, using the AddDbContext method.
+// The AddDbContext method configures the DataContext to use SQLite as the database provider, using the connection string retrieved from the application configuration.
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteDatingAppConnection"));
+});
+
 
 /*
 Now we have two sections, if you like, inside this file.
