373f96237cb83ae9b910718c205f3daded1494a5
bbadah <bilal.badah@capgemini.com>
2023-07-26 18:20:22 +0100

    61. Hooking up the register method to the service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -14,14 +14,14 @@ export class AccountService {
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   login(model: any) {
 
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map((response :User )=>{
+      map((response: User) => {
         const user = response;
-        if( user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -29,11 +29,35 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user : User) {
+  // Send a registration request to the server with the provided 'model' data.
+  // The 'model' parameter contains user registration details such as username, password, etc.
+
+  register(model: any) {
+    // Make a POST request to the server endpoint for user registration.
+    // The 'User' type is expected to represent the user information received from the server.
+
+    return this.http.post<User>(this.baseUrl + 'account/register', model)
+      .pipe(
+        // Use the 'map' operator to process the response from the server.
+        map(user => {
+          // If the 'user' object is truthy (not null or undefined),
+          // store it in the local storage as a JSON string under the "user" key.
+          // Also, update the 'currentUserSource' BehaviorSubject with the new user.
+
+          if (user) {
+            localStorage.setItem("user", JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+        })
+      );
+  }
+
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,28 +8,52 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  // The @Input decorator is used to receive data from the parent component (HomeComponent) into this component.
-  // The 'usersFromHomeComponent' property will be populated with data passed from the HomeComponent.
-  @Input() usersFromHomeComponent: any;
 
   // The @Output decorator is used to emit events from this component to the parent component (HomeComponent).
   // The 'cancelRegister' event is an EventEmitter that allows the parent component to subscribe and handle this event.
   @Output() cancelRegister = new EventEmitter();
 
-  model : any = {}  
-  
-  constructor() { }
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
 
+  // The 'register' method initiates the user registration process by calling the 'register' method of the accountService.
+  // After subscribing to the registration request, it handles the response using two callback functions: 'next' and 'error'.
+
   register() {
-    console.log(this.model);
+    // Call the 'register' method of the accountService to initiate the user registration process.
+    // The 'this.model' contains the user registration data (e.g., username, password).
+
+    this.accountService.register(this.model).subscribe({
+      // Handle the response after a successful registration (next callback).
+      // In this case, call the 'cancel' method to perform an action when the registration is successful.
+
+      next: () => {
+        this.cancel();
+      },
+
+      // Handle any errors that occur during the registration process (error callback).
+      // In this case, log the error to the console.
+
+      error: error => {
+        console.log(error);
+      }
+    });
   }
 
+  // The 'cancel' method is called when the user wants to cancel the registration process.
+  // It emits an event using the 'cancelRegister' EventEmitter to notify the parent component.
+
   cancel() {
+    // Emit the 'cancelRegister' event with the value 'false' to notify the parent component.
+    // The parent component (HomeComponent) can subscribe to this event and handle it accordingly.
+
     this.cancelRegister.emit(false);
   }
+
 
 }
