d33c56a7c18cd19ae2d6d3ae7330bdaad670bf78
bbadah <bilal.badah@capgemini.com>
2023-07-26 14:54:22 +0100

    58. Adding a register form

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  registerMode = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  registerToggle(){
+    this.registerMode = !this.registerMode;
+  }
+
+
+
+
+}
