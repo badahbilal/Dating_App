463404e5cc52b6c6af16217320f553de4735027e
bbadah <bilal.badah@capgemini.com>
2023-08-07 11:35:05 +0100

    68. Routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +11,11 @@ import { User } from '../_models/user';
 })
 export class NavComponent {
 
-  model : any = {};
+  model: any = {};
 
 
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -22,29 +23,40 @@ export class NavComponent {
 
 
   login() {
-    
-    // Perform user login by calling the login method of the accountService.
-    // The login details are passed in the 'model' object.
-    this.accountService.login(this.model).subscribe({
-      next: response => {
-        // Handle the response from the server after successful login.
-        // In this case, the response is logged to the console and the 'loggedIn' flag is set to true.
-        console.log(response);
 
+    // Perform user login by calling the 'login' method of the accountService.
+    // The login details are passed in the 'model' object.
+
+    this.accountService.login(this.model).subscribe({
+      // Handle the response after a successful login (next callback).
+      // In this case, navigate to the "/members" route using the Angular Router.
+
+      next: response => {
+        // Navigate to the "/members" route using the Angular Router.
+        // This is typically done after successful authentication.
+
+        this.router.navigateByUrl("/members");
       },
 
+      // Handle any errors that occur during the login process (error callback).
+      // In this case, log the error to the console.
+
       error: error => {
-        // Handle any errors that occur during the login process.
-        // In this case, the error is logged to the console.
         console.log(error);
       }
     });
+
   }
+
+  // The 'logout' function handles the user logout process.
 
   logout() {
-
+    // Call the 'logout' method of the accountService to perform user logout.
     this.accountService.logout();
 
+    // Navigate to the root path ("/") after logout.
+    this.router.navigateByUrl("/");
   }
-  
+
+
 }
