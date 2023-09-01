228a47d9300cc2fbf4ef6f0c4312c9695840cc73
bbadah <bilal.badah@capgemini.com>
2023-09-01 09:18:41 +0100

    124. Using the service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 // MemberService class handles HTTP requests to retrieve member data from the API.
 
@@ -10,22 +11,42 @@ import { Member } from '../_models/member';
 })
 export class MemberService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   // Get a list of members from the API.
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   // Get a specific member by username from the API.
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  //
+  // Update a member's data through the API.
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member)
+    // Make an HTTP PUT request to the API to update the member's data.
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      // Use the 'map' operator to update the local members array after a successful update.
+      map(() => {
+        // Find the index of the member being updated in the local 'members' array.
+        const index = this.members.findIndex(m => m.id === member.id);
+
+        // Update the local member object with the new data from the 'member' parameter.
+        // This ensures that the local data is kept in sync with the updated data.
+        this.members[index] = { ...this.members[index], ...member };
+      })
+    );
   }
 
 }
