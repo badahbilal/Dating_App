d6916907b6cc3c08719eaa4cd409eb289bbe2386
bbadah <bilal.badah@capgemini.com>
2023-09-02 13:19:07 +0100

    132. Using the Created At Route method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -52,49 +52,64 @@ namespace API.Controllers
         }
 
 
+        // HTTP PUT method to update a user's profile information.
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-
-
+            // Retrieve the user based on the authenticated username.
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
+            // Check if the user exists.
             if (user == null) return NotFound();
 
+            // Map the properties of 'memberUpdateDto' to the 'user' entity.
             _mapper.Map(memberUpdateDto, user);
 
+            // Save the updated user information.
             if (await _userRepository.SaveAllAsync()) return NoContent();
 
+            // If the update fails, return a BadRequest response.
             return BadRequest("Failed to update");
         }
 
+        // HTTP POST method to add a photo to a user's profile.
         [HttpPost("add-Photo")]
         public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
         {
-
+            // Retrieve the user based on the authenticated username.
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-            if( user == null ) return NotFound();
+            // Check if the user exists.
+            if (user == null) return NotFound();
 
+            // Upload the provided photo to a cloud service (e.g., Cloudinary).
             var result = await _photoService.AddPhotoAsync(file);
 
+            // Check if any errors occurred during the photo upload.
             if (result.Error != null) return BadRequest(result.Error.Message);
 
+            // Create a 'Photo' entity based on the upload result and user.
             var photo = new Photo
             {
                 Url = result.SecureUrl.AbsoluteUri,
                 PublicId = result.PublicId
             };
 
+            // Set the 'IsMain' flag for the photo if it's the user's first photo.
             if (user.Photos.Count == 0) photo.IsMain = true;
 
+            // Add the photo to the user's collection of photos.
             user.Photos.Add(photo);
 
+            // Save the updated user information, including the new photo.
             if (await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                // Return a created response with information about the added photo.
+                return CreatedAtAction(nameof(GetUser), new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
 
+            // If there's a problem adding the photo, return a BadRequest response.
             return BadRequest("Problem adding photo");
         }
-
     }
 }
